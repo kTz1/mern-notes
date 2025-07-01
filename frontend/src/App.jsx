3b2fda512a1abf900e862d9c67c373bd02bb1e9b
kTz1 <catalin_simon90@yahoo.com>
2025-07-01 16:55:01 +0300

    feat: add daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import CreateNote from "./pages/CreateNote";
-import NoteDetail from "./pages/NoteDetail";
+import NoteDetails from "./pages/NoteDetails";
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateNote />} />
-        <Route path="/create/:id" element={<NoteDetail />} />
+        <Route path="/create/:id" element={<NoteDetails />} />
       </Routes>
     </div>
   );

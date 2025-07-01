e833877e93dbcd53effa975dac29660a8bd371b1
kTz1 <catalin_simon90@yahoo.com>
2025-07-01 18:14:41 +0300

    feat: add CORS support, rate limiting UI, and note fetching functionalityadd Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import NoteDetails from "./pages/NoteDetails";
 
 const App = () => {
   return (
-    <div data-theme="forest">
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateNote />} />
-        <Route path="/create/:id" element={<NoteDetails />} />
+        <Route path="/note/:id" element={<NoteDetails />} />
       </Routes>
     </div>
   );
